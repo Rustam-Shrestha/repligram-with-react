97c7c8d188d3bc5fde72ff8dd025eff6bc83a3ff
Santosh <santosh.787402@smc.tu.edu.np>
2024-03-28 09:37:38 +0545

    implementing a basic middleware as a function in nodejs

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
-// we require express
-const  express = require("express");
+// Require express
+const express = require("express");
 const app = express();
 const PORT = 5000;
 
-// in each get request URL parameter is checked and executed
-app.get("/", (req,res)=>{
-    res.send("hello world");
-})
+// Define custom middleware
+const customMiddleware = (req, res, next) => {
+    var a = 10;
+    var b = 20;
+    var c = a * b;
+    console.log("Product is " + c);
+    //if not used next() website will just load infinitely
+    next(); // Call next to pass control to the next middleware or route handler
+};
 
-// Navigation link to the project in console
-app.listen(PORT, ()=>{
-    console.log("server is running in http://localhost:"+PORT)
-})
+// Use custom middleware
+app.use(customMiddleware);
+
+// Define route handler for the root URL
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+});
