fc663a2d8533654a0f603f9f8c9b580f77db1414
Santosh <santosh.787402@smc.tu.edu.np>
2024-03-29 22:53:17 +0545

    here we have completed mongodb database integration so when we hit /signup endpoint with thunderclient or postman passing value of header as email password and name it will take the value if they are given and not take if empty

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
 
 router.post("/signup",(req,res)=>{
     const {name, email, password} = req.body;
-    if(!name | !email | !password){
-        res.status(422).json({error:"give all the information"})
+    if(!name || !email || !password){
+        return res.status(422).json({error:"Please provide all the information"});
     }
-    res.json({messsage:"datas sent successfully"})
-    res.send("user authenthication site");
+    
+    User.findOne({email: email})
+        .then(savedUser => {
+            if(savedUser){
+                return res.status(422).json({error:"User already exists with that email"});
+            } else {
+                const newUser = new User({
+                    email, password, name
+                });
+
+                newUser.save()
+                    .then(user => {
+                        res.json({message:"User registered successfully"});
+                    })
+                    .catch(err => {
+                        console.error("Error saving user:", err);
+                        res.status(500).json({error: "Failed to register user"});
+                    });
+            }
+        })
+        .catch(err => {
+            console.error("Error finding user:", err);
+            res.status(500).json({error: "Failed to register user"});
+        });
 });
-router.get("/",(req,res)=>{
-    res.send("user authenthication site");
+  
+router.get("/", (req,res) => {
+    res.send("User authentication site");
 });
 
 module.exports = router;
